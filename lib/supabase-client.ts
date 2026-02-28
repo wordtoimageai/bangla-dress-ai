66f799e815d37ab29239781c7ac5fb5c4c07e074
wordtoimageai <wordtoimageai@gmail.com>
2026-03-01 01:20:10 +0600

    Add lib/supabase-client.ts

---
+++ lib/supabase-client.ts
@@ -0,0 +1,8 @@
+"use client";
+import { createBrowserClient } from "@supabase/ssr";
+
+export const createSupabaseClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
